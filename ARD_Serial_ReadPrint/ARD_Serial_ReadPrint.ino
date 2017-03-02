d544ca331da17f5da1d7ed651a116d0cdda5e49a
Zack Snyder <zsnyder317@yahoo.com>
2017-03-02 14:34:22 -0600

    Updated Debugging Arduino Code on Dev Branch
    Use to test sending values using UART.

--- ARD_Serial_ReadPrint/ARD_Serial_ReadPrint.ino
+++ ARD_Serial_ReadPrint/ARD_Serial_ReadPrint.ino
@@ -1,15 +1,36 @@
 /*
   Reads from serial and then prints
- */
+*/
 
+
+int busy = 8;
 void setup() {
   // initialize both serial ports:
   Serial.begin(9600);
+  pinMode(busy, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
+
+  digitalWrite(busy, LOW);
+  digitalWrite(13,LOW);
+  //while(!Serial.available()) {}
+
   if (Serial.available()) {
+
+    digitalWrite(busy, HIGH);
+    digitalWrite(13, HIGH);
+    
     int inByte = Serial.read();
     Serial.println(inByte);
+
+    for (int i = 0; i < 35; i++) {
+      if (Serial.available()) {
+        int inByte = Serial.read();
+        Serial.println(inByte);
+      }
+    }
+
   }
 }
