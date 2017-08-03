d4ed2801b601c8a402b9f0055312e7afa6dcd9d1
zssnyder <zsnyder317@yahoo.com>
2017-08-03 15:22:24 -0500

    modified:   UNITE_Main.X/SampleManager.h

--- UNITE_Main.X/SampleManager.h
+++ UNITE_Main.X/SampleManager.h
@@ -21,6 +21,7 @@ extern "C" {
 
 #endif	/* SAMPLEMANAGER_H */
 
+// Defines types of GPS sentence data
 typedef enum {
     Header,
     Time,
@@ -36,52 +37,67 @@ typedef enum {
     Done
 } GPSDataIndex;
 
+// Struct to store ADC sampling properties
 typedef struct {
     uint16_t channelSelect;
     int channelCount;
 } ADCSampleConfig;
 
+// Holds the raw data array sizes based on the instrument's sampling configs
 extern uint16_t LP_BUFFER_SIZE;
 extern uint16_t MAG_BUFFER_SIZE;
 
-// State Variables
+/* State Variables */
+
+// Sampling wait timers for each instrument
 extern unsigned long currentLangmuirProbeWait;
 extern unsigned long currentMagnetometerWait;
 extern unsigned long currentTemperatureWait;
 extern unsigned long currentGPSWait;
 
+// Used by sweeping timer
 extern int langmuirProbeCallbackCount;
 extern int magnetometerCallbackCount;
 
+// Sampling progress
 extern unsigned long currentLangmuirProbeSweepProgress;
 extern unsigned long currentMagnetometerSweepProgress;
 
+// Sampling state
 extern bool isLangmuirProbeSampling;
 extern bool isMagnetometerSweeping;
 extern bool shouldMagnetometerSample;
 
+// Did GPS obtain lock for last sample
 extern bool isGPSLocked;
 
-// Sampling Functions
+
+/* Sampling Functions */
+
+// Start sampling instruments
 void BeginTemperatureSampling();
 void BeginGPSSampling();
 void BeginMagnetometerSampling();
 void BeginLangmuirProbeSampling();
 
+// Stop sampling instruments
 void EndLangmuirProbeSampling();
 void EndMagnetometerSampling();
 void EndTemperatureSensorSampling();
 void EndGPSSampling();
 
+// Manages the Sweeping progress of an instrument (called by Timer 1)
 void ManageSweepingProgress();
 
+// Takes an individual sample of each instrument
 void TakeProbeSample();
 void TakeMagnetometerSample();
 void TakeTemperatureSample();
 int TakeGPSSample(int);
 
+// Parses a GPS sentence
 void ParseGPSSample();
 
-// Testing Functions
+// Testing/Debugging Functions
 void TransmitTestString();
 void TestDACSPI();
