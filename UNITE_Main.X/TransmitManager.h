127e716cbda9203f07fd21952608857b2661e480
zssnyder <zsnyder317@yahoo.com>
2017-08-03 16:01:23 -0500

    modified:   UNITE_Main.X/TransmitManager.h 	modified:   UNITE_Main.X/build/XC16_24FJ256GA110/production/SatelliteMode.o 	modified:   UNITE_Main.X/build/XC16_24FJ256GA110/production/mcc_generated_files/tmr2.o 	modified:   UNITE_Main.X/build/XC16_24FJ256GA110/production/mcc_generated_files/tmr3.o 	modified:   UNITE_Main.X/build/XC16_24FJ256GA110/production/mcc_generated_files/tmr4.o 	modified:   UNITE_Main.X/build/XC16_24FJ256GA110/production/mcc_generated_files/tmr5.o 	modified:   UNITE_Main.X/build/XC16_24FJ256GA110/production/mcc_generated_files/traps.o 	modified:   UNITE_Main.X/build/XC16_24FJ256GA110/production/mcc_generated_files/uart1.o 	modified:   UNITE_Main.X/dist/XC16_24FJ256GA110/production/memoryfile.xml

--- UNITE_Main.X/TransmitManager.h
+++ UNITE_Main.X/TransmitManager.h
@@ -21,39 +21,51 @@ extern "C" {
 
 #endif	/* TRANSMITMANAGER_H */
 
-#define SIMPLEX_RES_TIMEOUT     1000
+// Timeout values in seconds - Subject to change
+#define SIMPLEX_RES_TIMEOUT     100000
 #define DUPLEX_RES_TIMEOUT      100000
 
-
+// Boolean for whether UNITE is sending packets to a TransmissionUnit
 extern bool isSending;
+
+// Timeout flags
 extern bool simplexTimeoutFlag;
 extern bool duplexTimeoutFlag;
 
-extern uint8_t temperaturePowerSwitch;
 /**********************
   Transmission Methods
  **********************/
 
+// Transmits any data found in the sending queue
 void TransmitQueue();
+// Reads a byte from a TransmissionUnit
 uint8_t Read(TransmissionUnit unit);
+// Sends a byte to a TransmissionUnit
 void Send(uint8_t byte, TransmissionUnit unit);
+// Packages a system's data
 uint16_t PackageData(System, uint16_t, uint8_t *, uint16_t);
+// Returns true if EPS/Simplex busy line is high
 bool IsLineBusy();
 
+// Reads SMS commands from Duplex
 void HandleCommand();
 
 /****************
   Power Methods
  ****************/
 
+// Sends power switch values to EPS
 void TogglePowerSwitches();
+// Reads the echo packet back from EPS
 void ReadPowerSwitches();
 
+// Power switch setters
 void SetLangmuirProbePower(bool);
 void SetMagnetometerPower(bool);
 void SetTemperaturePower(bool);
 void SetGPSPower(bool);
 
+// Power switch getters
 bool isLangmuirProbeOn(void);
 bool isMagnetometerOn(void);
 bool isTemperatureOn(void);
