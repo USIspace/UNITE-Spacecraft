5439e2bed6b0754756fc139ae7aa09e53831cb93
zssnyder <zsnyder317@yahoo.com>
2017-08-03 14:29:13 -0500

    modified:   UNITE_Main.X/SatelliteMode.h 	modified:   UNITE_Main.X/build/XC16_24FJ256GA110/production/SatelliteMode.o 	modified:   UNITE_Main.X/build/XC16_24FJ256GA110/production/main.o 	modified:   UNITE_Main.X/build/XC16_24FJ256GA110/production/mcc_generated_files/tmr1.o 	modified:   UNITE_Main.X/build/XC16_24FJ256GA110/production/mcc_generated_files/tmr2.o 	modified:   UNITE_Main.X/build/XC16_24FJ256GA110/production/mcc_generated_files/tmr3.o 	modified:   UNITE_Main.X/build/XC16_24FJ256GA110/production/mcc_generated_files/tmr4.o 	modified:   UNITE_Main.X/build/XC16_24FJ256GA110/production/mcc_generated_files/tmr5.o 	modified:   UNITE_Main.X/dist/XC16_24FJ256GA110/production/memoryfile.xml

--- UNITE_Main.X/SatelliteMode.h
+++ UNITE_Main.X/SatelliteMode.h
@@ -1,12 +1,12 @@
 /* 
- * File:   InterimMode.h
+ * File:   SatelliteMode.h
  * Author: zacksnyder
  *
  * Created on February 28, 2017, 1:43 PM
  */
 
-#ifndef INTERIMMODE_H
-#define	INTERIMMODE_H
+#ifndef SATELLITEMODE_H
+#define	SATELLITEMODE_H
 
 #ifdef	__cplusplus
 extern "C" {
@@ -19,8 +19,9 @@ extern "C" {
 }
 #endif
 
-#endif	/* INTERIMMODE_H */
+#endif	/* SATELLITEMODE_H */
 
+// Defines the operational modes
 typedef enum {
     startup,
     interim,
@@ -29,25 +30,35 @@ typedef enum {
     safe
 } UNITEMode;
 
-
+// Struct to store operational mode properties
 typedef struct {
-    int startAltitudeInKm; // Altitude to begin sampling in this mode 
-    int endAltitudeInKm; // Altitude to end sampling and switch to new mode
-    unsigned long stopTime; 
+    int startAltitudeInKm;  // Altitude to begin sampling in this mode 
+    int endAltitudeInKm;    // Altitude to end sampling and switch to new mode
+    unsigned long stopTime; // In case GPS fails, uses this value to switch modes
 } SatelliteMode;
 
 
-
+// Global variable for current operational mode
 extern UNITEMode currentMode;
+// Global variable for total mission time in seconds
 extern unsigned long totalTime;
+// Global variable for time in minutes since 00:00 UTC
 extern double timeInMin;
+// Global variable to track when Duplex is connected
 extern bool isDuplexConnected;
+// Global variable to track the last GPS altitude reading
 extern unsigned long lastAltitude;
 
+// Intializes configuration values and main loop timer
 void Satellite_Initialize(void);
+// Returns the proper operational mode based on lastAltitude and totalTime
 UNITEMode UpdateMode(void);
 
+// Runs all the major functions of UNITE
+// Called by Timer 5
 void MainLoop(void);
 
+// Updates timeInMin from GPS time of day
 void SetTime(uint8_t *, int);
+// Updates lastAltitude from GPS altitude
 void SetAltitude(uint8_t *, int);
